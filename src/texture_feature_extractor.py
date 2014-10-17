3f7a9e771b2a06855f5c0f149b166b87f630764e
Sergeev Pavel <sergeev.pavel.al@gmail.com>
2014-10-18 03:35:55 +0400

    implemented some methods of TFE

--- src/texture_feature_extractor.py
+++ src/texture_feature_extractor.py
@@ -1,6 +1,8 @@
 import random
+import sys
 
 from numpy.linalg import norm
+from numpy.lib.function_base import average
 
 from feature_extractor import FeatureExtractor
 
@@ -11,24 +13,58 @@ class TextureFeatureExtractor(FeatureExtractor):
         self.tile_size = 50
         self.tiles_count = 100
         self.images_for_tailing_count = 500
-        self.delta = 40.0
+        self.delta = 80.0
+        self.debug = True
         self._generate_tiles()
 
     def extract(self, img):
-        return []
+        features = []
+        for tile in self.tiles:
+            features.append(self._img_tile_distance(img, tile))
 
-    def _tile_dist(self, tile1, tile2):
-        sum_dist = 0
-        diff = tile1 - tile2
-        for row in diff:
-            for pixel in row:
-                pixel /= 255
-                sum_dist += norm(pixel)
-        return sum_dist / (self.tile_size ** 2)
+    def _img_tile_distance(self, img, tile):
+        '''
+        distance between img and tile is defined as min of
+        distances between tile and all sub-images of img
+        :param img:
+        :param tile:
+        :return:
+        '''
+        (img_height, img_width) = img.shape[0:2]
+        (tile_height, tile_width) = tile.shape[0:2]
+        result = sys.float_info.max
+        for i in xrange(0, img_width - tile_width):
+            for j in xrange(0, img_height - tile_height):
+                sub_img = img[i:(i + tile_width), j:(j + tile_height)]
+                dst = self._sub_img_tile_distance(tile, sub_img)
+                result = min(dst, result)
+        return result
+
+    def _sub_img_tile_distance(self, tile, sub_img):
+        '''
+        distance between a sub-image and a texture tile are defined
+        as the maximum of Euclidian distance between their pixels in RGB
+        :param tile:
+        :param sub_img:
+        :return: distance
+        '''
+        return max(norm(tile.astype(float) - sub_img.astype(float), axis=2).flat)
+
+    def _tile_tile_distance(self, tile1, tile2):
+        '''
+        we define distance between two tiles as the average
+         Euclidian distance between the pixels of the tiles in RGB(0..1, 0..1, 0..1)
+        :param tile1:
+        :param tile2:
+        :return: distance in (0 ... sqrt(255**2 + 255**2 + 255**2))
+        '''
+        return average(norm(tile1.astype(float) - tile2.astype(float), axis=2))
 
     def _check_same_tile_exist(self, new_tile):
         for tile in self.tiles:
-            if self._tile_dist(tile, new_tile) < self.delta:
+            dst = self._tile_tile_distance(tile, new_tile)
+            self._debug_print('dst: {}'.format(dst))
+            if dst < self.delta:
                 return True
         return False
 
@@ -42,14 +78,18 @@ class TextureFeatureExtractor(FeatureExtractor):
             for new_tile in new_tiles:
                 if not self._check_same_tile_exist(new_tile):
                     self.tiles.append(new_tile)
+                    self._debug_print('tiles count: {}'.format(len(self.tiles)))
                     if len(self.tiles) == self.tiles_count:
                         return
 
     def _divide_img_by_tiles(self, img):
         tiles = []
-        height = img.shape[0]
-        width = img.shape[1]
+        (height, width) = img.shape[0:2]
         for x in xrange(0, width, self.tile_size):
             for y in xrange(0, height, self.tile_size):
                 tiles.append(img[x:(x + self.tile_size), y:(y + self.tile_size)])
         return tiles
+
+    def _debug_print(self, str):
+        if self.debug:
+            print('TFE debug: {}'.format(str))
