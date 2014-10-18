f52e0b818b8778986e76d37e92b17cb25191a741
Pavel Yurgin <payurgin@gmail.com>
2014-10-18 23:42:54 +0300

    add color feature extractor

--- src/color_feature_extractor.py
+++ src/color_feature_extractor.py
@@ -1,10 +1,89 @@
-import sys
-
-
+import cv2
 from feature_extractor import FeatureExtractor
+import numpy as np
+import warnings
 
 
-class TextureFeatureExtractor(FeatureExtractor):
-    def __init__(self, image_loader):
-        self.image_loader = image_loader
+class ColorFeatureExtractor(FeatureExtractor):
+    def __init__(self, n=10, chanel_h=10, chanel_s=10, chanel_v=10):
+        self._chanel_h = chanel_h
+        self._chanel_s = chanel_s
+        self._chanel_v = chanel_v
+        self.n = n
+
+    def extract(self, image):
+        if image.shape[0] < self.n or image.shape[1] < self.n:
+            raise Exception("size of image must be bigger then n")
+        if len(image) % self.n:
+            warnings.warn("Warning: n is not multiple of size of image")
+        feature_vector = []
+        for part_of_image in self._partition_image_generator(image):
+            feature_vector.extend(self._get_feature_vector(part_of_image).tolist())
+        return feature_vector
+
+    #
+    # def set_paremeter(self, n=1, _chanel_h=6, _chanel_s=6, _chanel_v=6):
+    # """
+    #     :param n:
+    #     :param _chanel_h:
+    #     :param _chanel_s:
+    #     :param _chanel_v:
+    #     """
+    #     self._chanel_h = _chanel_h
+    #     self._chanel_s = _chanel_s
+    #     self._chanel_v = _chanel_v
+    #     self.n = n
+
+    def _partition_image_generator(self, image):
+        """
+        :param image: square cv2 image
+        :return: part of image, converted to HSV color space
+        """
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV_FULL)
+        image_size = len(image)
+        part_size = image_size // self.n
+        for up_left, up_right in zip(xrange(0, image_size, part_size),
+                                     xrange(part_size, image_size + part_size, part_size)):
+            for down_left, down_right in zip(xrange(0, image_size, part_size),
+                                             xrange(part_size, image_size + part_size, part_size)):
+                yield hsv_image[up_left:up_right, down_left:down_right]
+
+    def _get_feature_vector(self, image):
+        """
+        :param image: image
+        :return: feature array of bool
+        """
+        pixel_feature = np.zeros((self._chanel_h, self._chanel_s, self._chanel_v), np.bool)
+
+        for i in xrange(image.shape[0]):
+            for j in xrange(image.shape[1]):
+                h, s, v = image[i, j]
+                h_chanel_coordinate = h // round(360. / self._chanel_h)
+                s_chanel_coordinate = s // round(256. / self._chanel_s)
+                v_chanel_coordinate = v // round(256. / self._chanel_v)
+                pixel_feature[h_chanel_coordinate, s_chanel_coordinate, v_chanel_coordinate] = 1
+        return pixel_feature.reshape((self._chanel_h * self._chanel_s * self._chanel_v))
+
+
+        # def _partirion_of_color_space(self):
+        #     _chanel_h_range = 360
+        #     _chanel_s_range = 256
+        #     _chanel_v_range = 256
+        #     _chanel_h_step = _chanel_h_range // self._chanel_h
+        #     _chanel_s_step = _chanel_s_range // self._chanel_s
+        #     _chanel_v_step = _chanel_v_range // self._chanel_v
+        #     _chanel_h_partition = [i for i in xrange(0 + _chanel_h_step, _chanel_h_range + _chanel_h_step, _chanel_h_step)]
+        #     _chanel_s_partition = [i for i in xrange(0 + _chanel_s_step, _chanel_s_range + _chanel_s_step, _chanel_s_step)]
+        #     _chanel_v_partition = [i for i in xrange(0 + _chanel_v_step, _chanel_v_range + _chanel_v_step, _chanel_s_step)]
+        #     return [_chanel_h_partition, _chanel_s_partition, _chanel_v_partition]
+        #
+        #
+        # def _images(self):
+        # '''
+        # :return: next available image in np.ndarray
+        #     '''
+        #     images = self.il.available_images()
+        #     for image in images:
+        #        yield self.il.load(image)
+
 
