8233154f43fb98c89bff205fb0eca689f6611513
avesloguzova <sashickk@gmail.com>
2014-10-27 12:53:13 +0300

    Rename solution container

--- src/solution_container.py
+++ src/solution_container.py
@@ -1,16 +1,22 @@
 __author__ = 'avesloguzova'
 
 
-class SolveContainer(object):
+class SolutionContainer(object):
     def __init__(self, classifier, features_extractor):
         self.classifier = classifier
         self.features_extractor = features_extractor
 
-    def serialize(self):
+    def serialize(self, filename):
         """
         Serialize object
         :return:
         """
+        self.features_extractor.serialize()
+        self.classifier.serialize()
+
+    def deserialize(self, filename):
+        self.features_extractor.deserialize()
+        self.classifier.deserialize()
 
     def get_classifier(self):
         return self.classifier
