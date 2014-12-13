cf346dcbf162ba877df537e78fb71877a4a38605
Pavel Yurgin <payurgin@gmail.com>
2014-12-13 18:36:52 +0400

    add decaf install script

---
+++ install_decaf.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/UCB-ICSI-Vision-Group/decaf-release.git
+cd decaf-release
+sudo apt-get install gfortran libopenblas-dev liblapack-dev
+
+pip install six
+pip install numpy
+sed -i.bak s/-Wl// decaf/layers/cpp/Makefile
+python setup.py build
+python setup.py install
